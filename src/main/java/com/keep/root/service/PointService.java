bb4ba13b7eec9e8c15cb73770ee77120279c36cb
juneglee <klcpop1@example.com>
2020-04-24 18:01:08 +0900

    2020-04-24

--- src/main/java/com/keep/root/service/PointService.java
+++ src/main/java/com/keep/root/service/PointService.java
@@ -1,8 +1,10 @@
 package com.keep.root.service;
 
 import java.util.List;
+import org.springframework.stereotype.Service;
 import com.keep.root.domain.Point;
 
+@Service
 public interface PointService {
 
   int add(Point point) throws Exception;
